50ced86b79c5ae3c1aa3bf2ee1fe0fefcc196d0c
Henning 'haenno' Beier <haenno@web.de>
2024-10-25 23:39:01 +0200

    wip

--- get-image.sh
+++ get-image.sh
@@ -1,11 +1,11 @@
-# run python script ./pycam/save_image.py to get image from camera
+# run python script ./save_image.py to get image from camera
 
 THIS_SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-python3 $THIS_SCRIPT_DIR/pycam/save_image.py
+python3 $THIS_SCRIPT_DIR/save_image.py
 
 # copy new_image.jpg to /var/www/html/image.jpg
-sudo cp -r $THIS_SCRIPT_DIR/pycam/new_image.jpg /var/www/html/image.jpg
+sudo cp -r $THIS_SCRIPT_DIR/new_image.jpg /var/www/html/image.jpg
 
 # copy again with timestamp to archive
 now=$(date "+%F_%H-%M-%S")
-sudo cp -r $THIS_SCRIPT_DIR/pycam/new_image.jpg /var/www/html/image_$now.jpg
+sudo cp -r $THIS_SCRIPT_DIR/new_image.jpg /var/www/html/image_$now.jpg
