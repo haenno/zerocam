4c9598be6bf07830bd9f1de171fad926dca2ff56
Henning 'haenno' Beier <haenno@web.de>
2024-10-25 23:43:34 +0200

    wip

--- save_image.py
+++ save_image.py
@@ -1,9 +1,7 @@
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
-config = picam2.create_still_configuration()
-config['resolution'] = (640, 480)
-config['rotation'] = 180
+config = picam2.create_still_configuration(main={"size": (800, 600)})
 picam2.configure(config)
 
 picam2.start()
