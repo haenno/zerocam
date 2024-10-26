ff24acf7ce9bf8bcba9d3879d5d6e5c9a94a2d08
Henning 'haenno' Beier <haenno@web.de>
2024-10-26 17:09:38 +0200

    wip

--- get-image.sh
+++ get-image.sh
@@ -3,19 +3,19 @@
 # */1 * * * * ~/zerocam/get-image.sh >> ~/zerocam/get-image.log 2>&1
 
 THIS_SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-python3 ~/save_image.py
+python3 ~/zerocam/save_image.py
 
 # copy new_image.jpg to /var/www/html/image.jpg
-sudo cp -r ~/new_image.jpg /var/www/html/image.jpg
+sudo cp -r ~/zerocam/new_image.jpg /var/www/html/image.jpg
 
 # copy again with timestamp to archive
 now=$(date "+%F_%H-%M-%S")
 # create folder if not exist: /var/www/html/images/
 sudo mkdir -p /var/www/html/images
-sudo cp -r ~/new_image.jpg /var/www/html/image_$now.jpg
+sudo cp -r ~/zerocam/new_image.jpg /var/www/html/image_$now.jpg
 
 # get last 100 log lines
-tail -n 100 ~/get-image.log >~/imagelog.txt
+tail -n 100 ~/zerocam/get-image.log >~/zerocam/imagelog.txt
 
 # copy to /var/www/html/imagelog.txt
-sudo cp -r ~/imagelog.txt /var/www/html/imagelog.txt
+sudo cp -r ~/zerocam/imagelog.txt /var/www/html/imagelog.txt
